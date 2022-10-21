10fe3658d08dec63ff8bc1948e0142dde3bf6b5b
artdemelo <contato.artdemelo@gmail.com>
2022-10-20 22:27:55 -0300

    correção no main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/design-system/";
+      config.base = "/first-design-system/";
     }
     return config;
   },
